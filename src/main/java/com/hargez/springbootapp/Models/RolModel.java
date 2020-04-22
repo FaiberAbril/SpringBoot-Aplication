23600991b519afcff37f729edb29bf8c84df3041
FaiberAbril <adrianfabril@gmail.com>
2020-04-21 23:14:31 -0500

    htlm plantalla terminada close #3

--- src/main/java/com/hargez/springbootapp/Models/RolModel.java
+++ src/main/java/com/hargez/springbootapp/Models/RolModel.java
@@ -25,6 +25,78 @@ public class RolModel {
 	public RolModel() {
 		// TODO Auto-generated constructor stub
 	}
+
+	public RolModel(int id, String description, String name) {
+		this.id = id;
+		this.description = description;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "RolModel [id=" + id + ", description=" + description + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RolModel other = (RolModel) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+	
 	
 	
 	
